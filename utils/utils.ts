0ff1c5372670a829459ac3a842080c5974df21ba
notcis <notcis07@gmail.com>
2025-09-16 16:03:28 +0700

    feat: add decrypt function for secure token handling

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,7 @@
 import { format } from "date-fns";
 import { fromZonedTime, toZonedTime } from "date-fns-tz";
 import qs from "query-string";
+import crypto from "crypto";
 
 export const convertZoneTimeToServer = (date: Date): Date => {
   return fromZonedTime(date, "UTC");
@@ -35,4 +36,20 @@ export function formUrlQuery({
       skipNull: true,
     }
   );
+}
+
+export function decrypt(token: string) {
+  try {
+    const encryptedText = token.replace(/ /g, "+");
+    const SECRET_KEY = Buffer.from(process.env.SECRET_KEY!, "base64"); // 32 bytes
+    const IV = Buffer.from(process.env.IV!, "base64"); // 16 bytes
+    const decipher = crypto.createDecipheriv("aes-256-cbc", SECRET_KEY, IV);
+    let decrypted = decipher.update(encryptedText, "base64", "utf8");
+    decrypted += decipher.final("utf8");
+    return decrypted;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    console.error("❌ Decrypt failed:", err.message);
+    return undefined;
+  }
 }
